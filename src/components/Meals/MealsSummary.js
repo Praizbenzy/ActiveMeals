1c70facb369b481657305e0b5e25444d692a0e9e
Praiz Benzy <skyflock@yahoo.com>
2021-11-18 17:20:27 +0100

    more changes made

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -3,7 +3,9 @@ import  './MealsSummary.scss';
 const MealsSummary = () => {
   return (
     <section className='summary'>
-      <h2>Delicious Food, Delivered To You</h2>
+      <p>Order From</p>
+      <h2>ChopNownow</h2>
+      <h3>Delicious Food, Delivered To You</h3>
       <p>
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
